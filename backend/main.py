016f771480b98f4ab68bfb27e5ae3d2b320bae91
Niyaz Miah <n.m@Niyazs-MacBook-Pro.local>
2025-02-02 17:46:14 +0000

    update

--- backend/main.py
+++ backend/main.py
@@ -2,18 +2,32 @@ from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from cryptography.fernet import Fernet
 from dotenv import load_dotenv
-import ssl
 import certifi
 
-# Load environment variables
+
+def decrypt_env():
+
+    with open("key.key", "rb") as key_file:
+        key = key_file.read()
+    fernet = Fernet(key)
+    with open(".env.enc", "rb") as enc_file:
+        encrypted_data = enc_file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(".env", "wb") as temp_env_file:
+        temp_env_file.write(decrypted_data)
+
+decrypt_env()
+
+
 load_dotenv()
 mongo_uri = os.getenv("MONGO_URI")
 
-# Initialize FastAPI app
+
 app = FastAPI()
 
-# Allow CORS for frontend
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -22,21 +36,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Use the connection string from the .env file
+
 client = MongoClient(
     mongo_uri,
     tlsCAFile=certifi.where()
 )
 
-db = client["FailedDocs"]  # Corrected to use the string "FailedDocs"
-collection = db["Docs"]  # Access the Docs collection within the FailedDocs database
+db = client["FailedDocs"]  
+collection = db["Docs"] 
 
 @app.get("/api/validation-status")
 async def get_validation_status():
-    result = collection.find({})
-    data = [{"_id": str(doc["_id"]), "error_type": doc["error_type"], "error_details": doc["error_details"]} for doc in result]
-    return {"validation_status": data}
-
+    try:
+        result = collection.find({})
+        data = [{"_id": str(doc["_id"]), "error_type": doc["error_type"], "error_details": doc["error_details"]} for doc in result]
+        return {"validation_status": data}
+    except Exception as e:
+        print(f"Database query failed: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 if __name__ == "__main__":
     import uvicorn
